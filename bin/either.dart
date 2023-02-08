7118e45bd123b5f1b618d23d75db34c0b5426331
Wladimir Bandeira <tecnico100@gmail.com>
2023-02-08 15:26:46 -0300

    modelo tradicional de código

--- bin/either.dart
+++ bin/either.dart
@@ -1,5 +1,11 @@
 import 'package:either/either.dart' as either;
 
 void main(List<String> arguments) {
-  print('Hello world!');
+  final list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  int sum = 0;
+  for (int i = 0; i < list.length; i++) {
+    final value = list[i];
+    sum = sum + value;
+  }
+  print(sum);
 }
