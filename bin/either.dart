b314fa6914848d335547129d70cd30411044934f
Wladimir Bandeira <tecnico100@gmail.com>
2023-02-08 16:33:11 -0300

    limpando o projeto

--- bin/either.dart
+++ bin/either.dart
@@ -1,14 +1,3 @@
 import 'package:either/either.dart' as either;
 
-void main(List<String> arguments) {
-  final list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  int sum = 0;
-  for (int i = 0; i < list.length; i++) {
-    final value = list[i];
-    sum = sum + value;
-  }
-  print("Valor do primeiro código ${sum}");
-
-  final sum2 = list.reduce((value, element) => value + element);
-  print("Valor do segundo código ${sum2}");
-}
+void main(List<String> arguments) {}
